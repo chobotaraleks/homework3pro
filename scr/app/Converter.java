aa8680e3eadfd6c4a310a3c2b5ab073c522915f2
chobotaraleks <79744088+chobotaraleks@users.noreply.github.com>
2024-11-08 01:16:31 +0200

    Update Converter.java

--- scr/app/Converter.java
+++ scr/app/Converter.java
@@ -4,8 +4,9 @@ public class Converter {
     public static double fahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
     }
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
 }
 
-public static double celsiusToFahrenheit(double celsius) {
-    return (celsius * 9 / 5) + 32;
-}
