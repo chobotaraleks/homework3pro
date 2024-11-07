d02c8afdf2a9c393bfe9ba9b897b0c46d781dbfe
chobotaraleks <chobotaraleks@gmail.com>
2024-11-08 00:31:54 +0200

    Initial commit: added project information output

---
+++ scr/app/Main.java
@@ -0,0 +1,7 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Project Converter: A simple temperature conversion application.");
+    }
+}
